ff6ac8d024aff1b2abd417d577aa453bf095cfba
Pedro Lucas <pedroolucas177@gmail.com>
2026-03-05 20:32:38 -0300

    Novo calculo

--- src/Desafio.java
+++ src/Desafio.java
@@ -5,5 +5,10 @@ public class Desafio {
             Isso é um teste para meu desafio do curso da alura
         """;
         System.out.println(texto);
+
+        int x = 4;
+        int y = 3;
+
+        System.out.println(x + y);
     }
 }
